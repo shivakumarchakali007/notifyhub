3ba8c6aeca4f28a052f33b2f5bf11c96855d4481
Shiva Kumar <=>
2026-05-24 15:53:58 +0530

    added the email unique constain at DB level

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_05_23_104557) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_24_101827) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -20,5 +20,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_05_23_104557) do
     t.string "name"
     t.string "password_digest"
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 end
