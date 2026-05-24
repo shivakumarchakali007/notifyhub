3d7ae6b1cc3e97135b68c4d305c834c3327aa7dc
Shiva Kumar <=>
2026-05-24 16:44:41 +0530

    [GREEN] Improve signup request specs and API error responses

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,2 +1,16 @@
 class Api::V1::AuthController < ApplicationController
+  def signup
+    user = User.new(user_params)
+    if user.save
+      render json: { success: true, data: { user: user } }, status: :created
+    else
+      render json: { success: false, errors: user.errors.full_messages }, status: :unprocessable_content
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
 end
--- spec/requests/api/v1/auth_spec.rb
+++ spec/requests/api/v1/auth_spec.rb
@@ -11,15 +11,57 @@ RSpec.describe "Api::V1::Auths", type: :request do
         }
       }
     end
-    it "creates a new user" do
+    context "with valid params" do
+      it "creates a new user" do
       expect {
         post '/api/v1/signup', params: valid_params
       }.to change(User, :count).by(1)
-    end
+      end
 
-    it "returns seccess response" do
+      it "returns seccess response" do
       post '/api/v1/signup', params: valid_params
       expect(response).to have_http_status(:created)
+      end
+    end
+
+    context "with invalid params" do
+      it "does not create a new user" do
+        invalid_params = valid_params.deep_dup
+        invalid_params[:user][:email] = nil
+        expect {
+          post '/api/v1/signup', params: invalid_params
+        }.to_not change(User, :count)
+      end
+
+      it "returns error response" do
+        invalid_params = valid_params.deep_dup
+        invalid_params[:user][:email] = nil
+        post '/api/v1/signup', params: invalid_params
+        expect(response).to have_http_status(:unprocessable_content)
+      end
+
+      it "does not allow duplicate email" do
+        User.create!(
+          name: "Existing User",
+          email: "shiva@example.com",
+          password: "password123"
+        )
+        expect {
+          post '/api/v1/signup', params: valid_params
+        }.to_not change(User, :count)
+      end
+
+      it 'returns error messages' do
+        invalid_params = valid_params.deep_dup
+        invalid_params[:user][:email] = nil
+
+        post '/api/v1/signup', params: invalid_params
+
+        json = JSON.parse(response.body)
+
+        expect(json['success']).to eq(false)
+        expect(json['errors']).to include("Email can't be blank")
+      end
     end
   end
 end
